1f85d9a8d3c6f35a8d5bca8e0302d833048bb138
KieranCrossland <kierancrossland1st@gmail.com>
2023-05-08 22:21:28 +0100

    changed exit() calls to return, and removed stdlib.h

--- main.c
+++ main.c
@@ -17,28 +17,28 @@ typedef struct
 
 typedef struct
 {
-    int centreX;
-    int centreY;
+    int posX;
+    int posY;
     float radius;
     int speedX;
     int speedY;
 }Ball;
 
-
 void DrawBall(Ball* ball)
 {
-    DrawCircle(ball->centreX, ball->centreY, ball->radius, WHITE);
+    DrawCircle(ball->posX, ball->posY, ball->radius, WHITE);
 }
+
 void UpdateBall(Ball* ball)
 {
-    ball->centreX += ball->speedX;
-    ball->centreY += ball->speedY;
+    ball->posX += ball->speedX;
+    ball->posY += ball->speedY;
 
-    if (ball->centreY + ball->radius >= GetScreenHeight() || ball->centreY - ball->radius <= 0)
+    if (ball->posY + ball->radius >= GetScreenHeight() || ball->posY - ball->radius <= 0)
     {
         ball->speedY *= -1;
     }
-    if (ball->centreX  - 200  + ball->radius >= GetScreenHeight() || ball->centreX - ball->radius <= 0)
+    if (ball->posX  - 200  + ball->radius >= GetScreenHeight() || ball->posX - ball->radius <= 0)
     {
         ball->speedX *= -1;
     }
@@ -48,27 +48,22 @@ void CheckPaddleOutOfBounds(Paddle* paddle) {
     //Checks if paddle are out of range. !DO NOT TOUCH, THIS BREAKS EASILY.
     if (paddle->posY < -1) paddle->posY = 1;
             if (paddle->posY >= SCREEN_HEIGHT - 80) paddle->posY = SCREEN_HEIGHT - 80;
-
             if (paddle->posY < -1) paddle->posY = 1;
             if (paddle->posY >= SCREEN_HEIGHT - 80) paddle->posY = SCREEN_HEIGHT - 80;
 }
+
 int main()
 {
-
-    //format: Paddle ExamplePaddle = { xPosition, yPosition, speed, health, width, height }
     Paddle Paddle1 = {SCREEN_WIDTH - 25, SCREEN_WIDTH/2, 6, 3, 20, 80};
     Paddle Paddle2 = {5, SCREEN_WIDTH/2, 6, 3, 20, 80};
-    //format: Ball ExampleBall = { xCentre, yCentre, radius, speedX, speedY};
-    Ball Ball1 = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20, 4, 4};
 
+    Ball Ball1 = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20, 4, 4};
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PongC");
     SetTargetFPS(60);
 
-
     while (!WindowShouldClose())
         {
-
 
             //Controls for player 1
             if (IsKeyDown(KEY_UP)) Paddle1.posY -= Paddle1.speed;
@@ -77,18 +72,15 @@ int main()
             if (IsKeyDown(KEY_W)) Paddle2.posY -= Paddle2.speed;
             if (IsKeyDown(KEY_S)) Paddle2.posY += Paddle2.speed;
 
-
             UpdateBall(&Ball1);
-
+            printf("%d %d\n",Ball1.posX, Ball1.posY);
             CheckPaddleOutOfBounds(&Paddle1);
 
             CheckPaddleOutOfBounds(&Paddle2);
 
-
-
             BeginDrawing();
 
-                DrawFPS(SCREEN_WIDTH - 100, 20);
+
 
                 ClearBackground(BLACK);
                 DrawText("Kieran's Pong in C",20,20,30,WHITE);
@@ -99,41 +91,11 @@ int main()
                 //Ball
                 DrawBall(&Ball1);
 
-
-
             EndDrawing();
         }
     CloseWindow();
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*
@@ -164,3 +126,4 @@ int BallMoveLeftX(Ball* ball)
     }
 }
 */
+
