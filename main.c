4451d79ef233f8b250a7d37bc0eb6fedd12ea440
KieranCrossland <kierancrossland1st@gmail.com>
2023-05-09 13:08:52 +0100

    removed VERSION and redundant paddle checks

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 #include "include/raylib.h"
 #include "include/raymath.h"
-#define VERSION "development"
+
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
@@ -45,13 +46,16 @@ void UpdateBall(Ball* ball)
 }
 
 void CheckPaddleOutOfBounds(Paddle* paddle) {
-    //Checks if paddle are out of range. !DO NOT TOUCH, THIS BREAKS EASILY.
+    //Checks if paddle are out of range
     if (paddle->posY < -1) paddle->posY = 1;
             if (paddle->posY >= SCREEN_HEIGHT - 80) paddle->posY = SCREEN_HEIGHT - 80;
             if (paddle->posY < -1) paddle->posY = 1;
-            if (paddle->posY >= SCREEN_HEIGHT - 80) paddle->posY = SCREEN_HEIGHT - 80;
 }
 
+void CheckBallTouchingPaddle(Ball* ball, Paddle* paddle)
+{
+
+}
 int main()
 {
     Paddle Paddle1 = {SCREEN_WIDTH - 25, SCREEN_WIDTH/2, 6, 3, 20, 80};
@@ -61,21 +65,21 @@ int main()
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PongC");
     SetTargetFPS(60);
-
+    fork();
     while (!WindowShouldClose())
         {
 
             //Controls for player 1
             if (IsKeyDown(KEY_UP)) Paddle1.posY -= Paddle1.speed;
             if (IsKeyDown(KEY_DOWN)) Paddle1.posY += Paddle1.speed;
-            //Controls for player2
+            //Controls for player 2
             if (IsKeyDown(KEY_W)) Paddle2.posY -= Paddle2.speed;
             if (IsKeyDown(KEY_S)) Paddle2.posY += Paddle2.speed;
 
             UpdateBall(&Ball1);
             printf("%d %d\n",Ball1.posX, Ball1.posY);
-            CheckPaddleOutOfBounds(&Paddle1);
 
+            CheckPaddleOutOfBounds(&Paddle1);
             CheckPaddleOutOfBounds(&Paddle2);
 
             BeginDrawing();
@@ -83,7 +87,7 @@ int main()
 
 
                 ClearBackground(BLACK);
-                DrawText("Kieran's Pong in C",20,20,30,WHITE);
+                DrawText("Kieran's Pong in C",40,20,30,YELLOW);
                 //Player 1
                 DrawRectangle(Paddle1.posX, Paddle1.posY, Paddle1.width, Paddle1.height, DARKBLUE);
                 //Player 2
@@ -96,34 +100,3 @@ int main()
     CloseWindow();
 
 }
-
-
-/*
-int BallMoveRightX(Ball* ball)
-{
-    ball->speed = 2;
-
-    if (ball->direction_is_left == false) {
-            printf("BallMoveRight\n");
-            ball->centreX += ball->speed;
-            if (ball->centreX >= SCREEN_WIDTH - 40) {
-                ball->direction_is_left = true;
-            }
-    }
-}
-int BallMoveLeftX(Ball* ball)
-{
-    ball->speed = 1;
-
-    if (ball->direction_is_left == true) {
-            printf("BallMoveLeft\n");
-            ball->centreX -= ball->speed;
-            if (ball->centreX -= ball->speed) {
-                if (ball->centreX <=  40) {
-                    ball->direction_is_left = false;
-                }
-            }
-    }
-}
-*/
-
