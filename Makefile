ec9f43eccb1e06b5797fdbc86039f930f5b6af74
KieranCrossland <kierancrossland1st@gmail.com>
2023-05-14 22:46:32 +0100

    changed the title text from inbuilt font to the nds bios font ttf

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BUILD_DIR=makebuilds
+BUILD_DIR=build
 
 windows:
 	gcc *.c -Wall -std=c99 -Wno-missing-braces -L lib/ms-windows -lraylib -lopengl32 -lgdi32 -lwinmm -o $(BUILD_DIR)/pong-c-windows
@@ -6,7 +6,7 @@ linux:
 	gcc *.c -Wall -std=c99 -Wno-missing-braces -L lib/gnu-linux -lraylib -lglfw -lGL -lm -lpthread -ldl -lrt -lX11 -o $(BUILD_DIR)/pong-c-linux
 
 mac:
-	clang --std=c99 *.c -L lib/mac-osx -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a -o $(BUILD_DIR)pong-c-macosx
+	clang --std=c99 *.c -L lib/mac-osx -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/mac-osx -o $(BUILD_DIR)pong-c-macosx
 
 clean:
 	rm $(BUILD_DIR)/*
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "include/raylib.h"
-
+#include <unistd.h>
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define WINDOW_FPS 144
@@ -47,46 +47,49 @@ void CheckPaddleOutOfBounds(Paddle* paddle) {
             if (paddle->pos.y < -1) paddle->pos.y = 1;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-     //Vector2 pos.x/ /*Vector2 pos.y/ /float speed/ /float width/ /float height/
-    Paddle Paddle1 = {WINDOW_WIDTH - 25.0, WINDOW_WIDTH/2.0 - 150, 4, 20, 90};
-    Paddle Paddle2 = {5.0, WINDOW_WIDTH/2.0 - 150, 4, 20, 90};
+    printf("Re-linking game objects\n");
 
+    Paddle player1 = {WINDOW_WIDTH - 25.0, WINDOW_WIDTH/2.0 - 150, 4, 20, 90};
+    Paddle player2 = {5.0, WINDOW_WIDTH/2.0 - 150, 4, 20, 90};
     Ball Ball1 = { /*Vector2 pos.x*/ WINDOW_WIDTH / 2.0, /*Vector2 pos.y*/ WINDOW_HEIGHT / 2.0, /*float radius*/ 20, /*int speedX*/ 2, /*int speedY*/ 2};
 
+    const char msg[256] ="Pong in C";
+
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "pong-c (raylib)");
+    Font ndsbios = LoadFontEx("assets/fonts/nintendo_ds_bios/Nintendo-DS-BIOS.ttf",36 ,0, 255);
 
     SetTargetFPS(WINDOW_FPS);
-
     while (!WindowShouldClose())
         {
 
             //Controls for player 1
-            if (IsKeyDown(KEY_UP)) Paddle1.pos.y -= Paddle1.speed;
-            if (IsKeyDown(KEY_DOWN)) Paddle1.pos.y += Paddle1.speed;
+            if (IsKeyDown(KEY_UP)) player1.pos.y -= player1.speed;
+            if (IsKeyDown(KEY_DOWN)) player1.pos.y += player1.speed;
             //Controls for player 2
-            if (IsKeyDown(KEY_W)) Paddle2.pos.y -= Paddle2.speed;
-            if (IsKeyDown(KEY_S)) Paddle2.pos.y += Paddle2.speed;
+            if (IsKeyDown(KEY_W)) player2.pos.y -= player2.speed;
+            if (IsKeyDown(KEY_S)) player2.pos.y += player2.speed;
 
 
             UpdateBall(&Ball1);
 
-            CheckPaddleOutOfBounds(&Paddle1);
-            CheckPaddleOutOfBounds(&Paddle2);
-
+            CheckPaddleOutOfBounds(&player1);
+            CheckPaddleOutOfBounds(&player2);
             BeginDrawing();
 
                 ClearBackground(BLACK);
-                DrawText("Pong in C",40,20,30,YELLOW);
-                DrawRectangle(Paddle1.pos.x, Paddle1.pos.y, Paddle1.width, Paddle1.height, DARKBLUE);
-                DrawRectangle(Paddle2.pos.x, Paddle2.pos.y, Paddle2.width, Paddle2.height, DARKGREEN);
+                DrawTextEx(ndsbios, msg, (Vector2){10.0f, 10.0f},(float)ndsbios.baseSize, 2, YELLOW);
+                DrawRectangle(player1.pos.x, player1.pos.y, player1.width, player1.height, DARKBLUE);
+                DrawRectangle(player2.pos.x, player2.pos.y, player2.width, player2.height, DARKGREEN);
                 DrawBall(&Ball1);
 
             EndDrawing();
         }
     CloseWindow();
     printf("Goodbye\n");
-    while(1);
 
 }
+
+
+
