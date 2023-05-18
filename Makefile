24a71f120944356cc4462774552f097d9035222c
KieranCrossland <kierancrossland1st@gmail.com>
2023-05-18 20:18:13 +0100

    yeah, changed something idk what

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ BUILD_DIR=build
 
 windows:
 	gcc *.c -Wall -std=c99 -Wno-missing-braces -L lib/ms-windows -lraylib -lopengl32 -lgdi32 -lwinmm -o $(BUILD_DIR)/pong-c-windows
+
 linux:
 	gcc *.c -Wall -std=c99 -Wno-missing-braces -L lib/gnu-linux -lraylib -lglfw -lGL -lm -lpthread -ldl -lrt -lX11 -o $(BUILD_DIR)/pong-c-linux
 
--- pong.c
+++ pong.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "include/raylib.h"
-#include "include/pong-c/pong.h"
-#include <unistd.h>
+
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define WINDOW_FPS 144
+
+/*Type declarations*/
+typedef struct
+{
+    Vector2 pos;
+    float speed;
+    float width;
+    float height;
+
+} Paddle;
+
+typedef struct
+{
+    Vector2 pos;
+    float radius;
+    Vector2 speed;
+}Ball;
+
+/*Beginning of code*/
 
 void DrawBall(Ball* ball)
 {
@@ -25,7 +45,7 @@ void UpdateBall(Ball* ball)
 }
 
 void CheckPaddleOutOfBounds(Paddle* paddle) {
-    //Checks if paddle are out of range
+    //Checks if a paddle's position exceeds the window bounds and stops it.
     if (paddle->pos.y < -1) paddle->pos.y = 1;
             if (paddle->pos.y >= WINDOW_HEIGHT - 80) paddle->pos.y = WINDOW_HEIGHT - 80;
             if (paddle->pos.y < -1) paddle->pos.y = 1;
@@ -33,16 +53,9 @@ void CheckPaddleOutOfBounds(Paddle* paddle) {
 
 int main(int argc, char* argv[])
 {
-    Config game_settings = {true, true};
-
     if (argc > 1)
     {
         printf("program arguemts: %s\n",argv[1]);
-        if (strcmp(argv[1],"--use_ttf=false") == 0)
-        {
-        game_settings.use_ttf = false;
-        printf("game_settings.use_ttf = false\n");
-        }
     }
     else
     {
@@ -53,7 +66,7 @@ int main(int argc, char* argv[])
     Paddle player2 = {5.0, WINDOW_WIDTH/2.0 - 150, 4, 20, 90};
     Ball Ball1 = { /*Vector2 pos.x*/ WINDOW_WIDTH / 2.0, /*Vector2 pos.y*/ WINDOW_HEIGHT / 2.0, /*float radius*/ 20, /*int speedX*/ 2, /*int speedY*/ 2};
 
-    const char msg[9] ="Pong in C";
+    const char msg[] ="Pong in C";
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "pong-c (raylib)");
     Font ndsbios = LoadFontEx("assets/fonts/nintendo_ds_bios/Nintendo-DS-BIOS.ttf",36 ,0, 255);
@@ -69,7 +82,6 @@ int main(int argc, char* argv[])
             if (IsKeyDown(KEY_W)) player2.pos.y -= player2.speed;
             if (IsKeyDown(KEY_S)) player2.pos.y += player2.speed;
 
-
             UpdateBall(&Ball1);
 
             CheckPaddleOutOfBounds(&player1);
@@ -77,23 +89,18 @@ int main(int argc, char* argv[])
             BeginDrawing();
 
                 ClearBackground(BLACK);
-                if (game_settings.use_ttf == false)
-                {
-                    DrawText("Pong in C",40,20,30,YELLOW);
-                }
-                else
-                {
-                    DrawTextEx(ndsbios, msg, (Vector2){10.0f, 10.0f},(float)ndsbios.baseSize, 2, YELLOW);
-                }
+                
+                DrawTextEx(ndsbios, msg, (Vector2){10.0f, 10.0f},(float)ndsbios.baseSize, 2, YELLOW);
 
                 DrawRectangle(player1.pos.x, player1.pos.y, player1.width, player1.height, DARKBLUE);
                 DrawRectangle(player2.pos.x, player2.pos.y, player2.width, player2.height, DARKGREEN);
+                
                 DrawBall(&Ball1);
 
             EndDrawing();
         }
     CloseWindow();
     printf("Goodbye\n");
+    return 0;
 
 }
-
